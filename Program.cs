21299c82af3124b7bd2038c23dc80efda832abdb
Hoanhuy2003 <hoan455554@gmail.com>
2026-06-20 13:35:57 +0700

    chore: sync database configuration

--- Program.cs
+++ Program.cs
@@ -5,14 +5,21 @@ using ceramic.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+Console.WriteLine("CONNECTION: " + connection);
+
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options => {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(
+        "Server=DESKTOP-35TNH6D;Database=ceramicshop;User Id=sa;Password=123456;TrustServerCertificate=True;Encrypt=False"
+    );
 });
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
@@ -23,9 +30,7 @@ builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<OrderService>();
 builder.Services.AddScoped<StatisticsService>();
 
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
